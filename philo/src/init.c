f81f20829545f85aeb8408af00880f12626bf27e
willianportilho <willianportilho01@gmail.com>
2022-11-23 16:31:02 -0300

    fixed alloc philos

--- philo/src/init.c
+++ philo/src/init.c
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 12:56:14 by beatriz           #+#    #+#             */
-/*   Updated: 2022/11/23 14:08:42 by wportilh         ###   ########.fr       */
+/*   Updated: 2022/11/23 16:30:12 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ void    init(t_data *data)
 
     count = 0;
     data->i = 0;
-    data->n_philo = 2;
-    data->philo = ft_calloc(2, sizeof(t_philo));
+    data->n_philo = 6;
+    data->philo = ft_calloc(data->n_philo, sizeof(t_philo));
     while(count < data->n_philo)
     {
         data->philo[count].philo = ft_calloc(1, sizeof(pthread_t));
