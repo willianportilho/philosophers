5a9615684fd44643100c8d79e89aeb6ae947494d
wportilh <willianportilho01@gmail.com>
2022-12-09 23:49:21 +0100

    removed drop_a_fork (useless/redundant function).

--- philo/src/philo_life.c
+++ philo/src/philo_life.c
@@ -6,16 +6,11 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 19:24:43 by wportilh          #+#    #+#             */
-/*   Updated: 2022/12/09 23:36:29 by wportilh         ###   ########.fr       */
+/*   Updated: 2022/12/09 23:46:59 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
-
-static void	drop_a_fork(pthread_mutex_t *fork)
-{
-	pthread_mutex_unlock(fork);
-}
 
 static int	take_a_fork(pthread_mutex_t *fork, t_philo *ph)
 {
@@ -41,11 +36,11 @@ void	*life(void *philo)
 	{
 		pthread_mutex_unlock(&ph->data->times_ate[ph->id - 1]);
 		if (take_a_fork(&ph->data->forks[ph->fork_left], ph) != TRUE)
-			return (drop_a_fork(&ph->data->forks[ph->fork_left]), NULL);
+			return (pthread_mutex_unlock(&ph->data->forks[ph->fork_left]), NULL);
 		if (take_a_fork(&ph->data->forks[ph->fork_right], ph) != TRUE)
 		{
-			drop_a_fork(&ph->data->forks[ph->fork_left]);
-			drop_a_fork(&ph->data->forks[ph->fork_right]);
+			pthread_mutex_unlock(&ph->data->forks[ph->fork_left]);
+			pthread_mutex_unlock(&ph->data->forks[ph->fork_right]);
 			return (NULL);
 		}
 		print_status_msg("is eating", ph);
@@ -54,16 +49,16 @@ void	*life(void *philo)
 		if (ph->data->die == TRUE)
 		{
 			pthread_mutex_unlock(&ph->data->die_mutex);
-			drop_a_fork(&ph->data->forks[ph->fork_left]);
-			drop_a_fork(&ph->data->forks[ph->fork_right]);
+			pthread_mutex_unlock(&ph->data->forks[ph->fork_left]);
+			pthread_mutex_unlock(&ph->data->forks[ph->fork_right]);
 			return (FALSE);
 		}
 		pthread_mutex_unlock(&ph->data->die_mutex);
 		pthread_mutex_lock(&ph->data->time_to_die_cur[ph->id - 1]);
 		ph->time_to_die_cur = current_time() + ph->data->time_to_die;
 		pthread_mutex_unlock(&ph->data->time_to_die_cur[ph->id - 1]);
-		drop_a_fork(&ph->data->forks[ph->fork_left]);
-		drop_a_fork(&ph->data->forks[ph->fork_right]);
+		pthread_mutex_unlock(&ph->data->forks[ph->fork_left]);
+		pthread_mutex_unlock(&ph->data->forks[ph->fork_right]);
 		pthread_mutex_lock(&ph->data->times_ate[ph->id - 1]);
 		if (--ph->times_ate == 0)
 		{
@@ -93,12 +88,12 @@ void	*life_one(void *philo)
 	ph = (t_philo *) philo;
 	ph->time_to_die_cur = ph->data->initial_time + ph->data->time_to_die;
 	if (take_a_fork(&ph->data->forks[ph->fork_right], ph) != TRUE)
-		return (drop_a_fork(&ph->data->forks[ph->fork_right]), NULL);
+		return (pthread_mutex_unlock(&ph->data->forks[ph->fork_right]), NULL);
 	while (1)
 	{
 		if (ph->time_to_die_cur <= current_time())
 		{
-			drop_a_fork(&ph->data->forks[ph->fork_right]);
+			pthread_mutex_unlock(&ph->data->forks[ph->fork_right]);
 			print_status_msg("die", ph);
 			return (NULL);
 		}
