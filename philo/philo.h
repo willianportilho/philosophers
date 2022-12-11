90242de14ab203d3c0a432758a0399c3f4245053
wportilh <willianportilho01@gmail.com>
2022-12-11 02:29:46 +0100

    removed LAG macro (useless)

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 19:37:26 by wportilh          #+#    #+#             */
-/*   Updated: 2022/12/11 00:09:46 by wportilh         ###   ########.fr       */
+/*   Updated: 2022/12/11 02:29:08 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@
 # define FALSE 0
 # define LIMITLESS -1
 # define FINISH_EAT 0
-# define LAG 1000
 
 typedef struct s_philo
 {
