9a939c73decd330dc1844eada4f746a670c3b3a5
wportilh <willianportilho01@gmail.com>
2022-12-11 03:01:40 +0100

    optimization (main.c).

--- philo/1_main.c
+++ philo/1_main.c
@@ -6,7 +6,7 @@
 /*   By: wportilh <wportilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 19:35:34 by wportilh          #+#    #+#             */
-/*   Updated: 2022/12/10 02:59:44 by wportilh         ###   ########.fr       */
+/*   Updated: 2022/12/11 02:53:03 by wportilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@ int	main(int argc, char *argv[])
 {
 	t_data		data;
 
-	if (init(argc, argv, &data) != TRUE)
-		return (FALSE);
-	if (data.n_philos == 1)
-		start_one_philo(&data);
-	else
-		start(&data);
-	finish(&data);
+	if (init(argc, argv, &data) == TRUE)
+	{
+		if (data.n_philos == 1)
+			start_one_philo(&data);
+		else
+			start(&data);
+		finish(&data);
+	}
 	clean(&data);
 	return (0);
 }
